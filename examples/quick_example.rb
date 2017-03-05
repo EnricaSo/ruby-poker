7232450ed068ac1b8be13c72c630b7ff87dba10b
don4get <anthonydeb4@msn.com>
2017-03-05 19:04:20 +0100

    New example

--- examples/quick_example.rb
+++ examples/quick_example.rb
@@ -3,9 +3,12 @@ require './lib/ruby-poker'
 
 hand1 = PokerHand.new("8H 9C TC JD QH")
 hand2 = PokerHand.new(["3D", "3C", "3S", "KD", "AH"])
-puts hand1
-puts hand1.just_cards
-puts hand1.rank
-puts hand2
-puts hand2.rank
+# puts hand1
+# puts hand1.just_cards
+# puts hand1.rank
+# puts hand2
+# puts hand2.rank
 puts hand1 > hand2
+
+hand3 = PokerHand.new(["4D", "4C", "4S", "4D", "AH"])
+puts hand3 > hand2
