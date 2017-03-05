4b0dd2fbc5ea669b11d72de47add8d0d8cf84cd0
don4get <anthonydeb4@msn.com>
2017-03-05 22:33:25 +0100

    clean quick_example.rb

--- examples/quick_example.rb
+++ examples/quick_example.rb
@@ -6,19 +6,5 @@ hand2 = PokerHand.new(["3D", "3C", "3S", "KD", "AH"])
 # puts hand1
 # puts hand1.just_cards
 # puts hand1.rank
-# puts hand2
-# puts hand2.rank
 
 puts hand1 > hand2
-
-hand3 = PokerHand.new(["4D", "4C", "4S", "4D", "AH"])
-puts hand3 > hand2
-hand4 = PokerHand.new(["AD", "AC", "AS", "4D", "AH"])
-puts hand4 > hand2
-
-# puts hand1 > hand2
-
-hand5 = PokerHand.new(["2D", "2C", "2S", "KD", "AH"])
-# Comment on branch Chica
-# Third comment on branch Chico
-# Second comment on branch Chica
