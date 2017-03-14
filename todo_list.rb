add82f5e030a46398c8f35eb400ecb4f97ac8d6d
don4get <anthonydeb4@msn.com>
2017-03-14 23:24:51 +0100

    todo list added

--- todo_list.rb
+++ todo_list.rb
@@ -1,3 +1,5 @@
 # TODO: probability at each step of the game :  preflop, flop, turn, river
-# TODO create an interface for the software
-# TODO look for data within winamax or pokerstar
+# TODO: create an interface for the software
+# TODO: look for data within winamax or pokerstar
+# TODO: look for information about a gamer and give him a risk Rank
+# TODO: develop the IA
