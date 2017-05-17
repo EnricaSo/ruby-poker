fe9fcab4c12ad614ee1d77a43730743bf95ed9b0
don4get <anthonydeb4@msn.com>
2017-05-17 23:23:49 +0200

    tips and tricks added

--- todo_list.rb
+++ todo_list.rb
@@ -5,3 +5,11 @@
 # TODO: develop the IA
 
 # TODO: Implement these evaluators: https://paginas.fe.up.pt/~niadr/PUBLICATIONS/LIACC_publications_2011_12/pdf/CN10_Estimating_Probability_Winning_LFT.pdf
+
+
+# list of gems to download:
+# awesome_print => colored print
+#
+a= "hello Chica"
+require 'awesome_print'
+ap [1,2,3]
