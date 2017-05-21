d9b4f2fb9e741369716d58debcc093d66c58a8a7
don4get <anthonydeb4@msn.com>
2017-05-21 18:11:48 +0200

    few more tips and tricks...

--- examples/tips_and_tricks.rb
+++ examples/tips_and_tricks.rb
@@ -1,12 +1,16 @@
 options = {
-	"Regexp is GOOD, intelligence is BETTER" 			=> 1,
-	"Several modules for the same method name" 			=> 2,
-	"Act depending on the future in a loop function" 	=> 3,
-	"Zipping two arrays to a hash in a blink"			=> 4,
-	"Build an array with an enumerable..." 				=> 5,
-	"Easiest tracking method method...of the universe"	=> 6,
-	"Prepending string instead of creating new ones" 	=> 7,
-	"Easy search for regexp"							=> 8
+	"Regexp is GOOD, intelligence is BETTER" 					=> 1,
+	"Several modules for the same method name" 					=> 2,
+	"Act depending on the future in a loop function" 			=> 3,
+	"Zipping two arrays to a hash in a blink"					=> 4,
+	"Build an array with an enumerable..." 						=> 5,
+	"Easiest tracking method method...of the universe"			=> 6,
+	"Prepending string instead of creating new ones" 			=> 7,
+	"Easy search for regexp"									=> 8,
+	"Only one block per function, so let's convert it to procs"	=> 9,
+	"Strange ways to call procs..."													=> 10,
+	"Closures"													=> 11,
+
 }
 p "Choose a trick:"
 options.each { |key, value| p "#{value}) #{key}"}
@@ -102,4 +106,57 @@ when 8
 	print "Full result : ", str[regexp], "\n"
 	/ica/.match(str)
 	puts $~ # only useful when writing ruby in console (irb mode)
+	/(?<e>E)/ =~ str # We have to change the style in ordre to store variables, but, still, it's simple and elegant
+	puts e
+when 9
+	arr = [1, 2, 3, 4]
+	arr.each { |i| p i}
+
+	def run_block
+		yield if block_given? #otherwise, it needs THE block (no more than one block can be yielded...)
+	end
+
+	run_block { puts "As .each, run_block can interprete blocks"}
+	run_block
+
+	class Array
+		def random_each(proc, &block) # Only one block, so we can convert them to Procs if there are many : proc = &block
+			shuffle.each do |e|
+				proc.call e
+				block.call e #here block is in fact a proc, converted 3 lines above...
+			end
+		end
+	end
+
+	p "random_each, within Array class : "
+	proc = Proc.new { |e| puts e}
+	[1,2,3,4].random_each(proc) { |e| puts "block effect"}
+
+when 10
+	# include 'Time'
+	puts Time.now
+	puts_parity = Proc.new {|int| int.even? ?
+		puts("#{int} is even") : puts("#{int} is odd")}
+	puts_parity.call(1)
+	puts_parity.(3)
+	puts_parity[4]
+	puts_parity === 42 #why this way of calling procs?!?
+
+	special_date = Proc.new{|date| date == Time.new(2017,6,5)}
+
+
+	reminderproc = Proc.new{|date|
+		case date
+		when special_date # in fact ruby understands
+						  # date === special_date <=> special_date === date <=> special_date.(date)
+			p "#{date} is a special date!! \nREMINDED : Be kind with Chica, for once"
+		else
+			p "#{date}... Kick her ass!!!!!!!! Mouhahaha http://i3.kym-cdn.com/photos/images/original/000/544/643/ece.jpg"
+		end
+	}
+
+	date = Time.new(2017,6,5)
+	reminderproc.(date)
+	reminderproc.(date + (60*60*24))
+
 end
